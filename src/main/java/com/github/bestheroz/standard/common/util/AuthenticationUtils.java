f4d263056a4a162195402f3859570379f07823a4
joony.kim <bestheroz@gmail.com>
2024-09-09 17:29:43 +0900

    ♻️

--- src/main/java/com/github/bestheroz/standard/common/util/AuthenticationUtils.java
+++
@@ -1,8 +0,0 @@
-package com.github.bestheroz.standard.common.util;
-
-import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@UtilityClass
-public class AuthenticationUtils {}
